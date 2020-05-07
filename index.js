a5bb43e5edcba5f96b7d480f04315594b0e7c8d7
Kerim Bektasevic <48659111+Kerim246@users.noreply.github.com>
2020-05-07 18:49:50 +0200

    Gradovi

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    if (req.method == 'GET') {
+        fs.readFile('gradovi.csv', (err, data) => {
+            if (err) return console.error(err);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            let spisak = data.toString().split("\n"); // jedna osoba jedan red
+            res.write('<table>');
+            res.write('<tr><th>Gradovi:</th>');
+            for (let i = 0; i < spisak.length - 1; i++) {
+                res.write('<tr><td>' + spisak[i] + '</td>');
+                //    res.write(spisak[i]);
+                res.write('</tr>');
+            }
+            res.write('</table>');
+            // res.end(JSON.stringify(niz + "\n"));  // Konvertovanje objekta u string 
+        });
+    }
+}).listen(8000);
